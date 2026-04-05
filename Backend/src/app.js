abbfee9925b94c5ba7031a0fd48a156ed68f2c56
Anupam Gavhane <anupamgavhanex@gmail.com>
2026-04-05 16:28:04 +0530

    Add frontend URL to CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: "http://localhost:5173", // Vite default
+  origin: ["http://localhost:5173","https://sic-gules.vercel.app/"], // Vite default
   credentials: true
 }));
 
